f177a285f1aa949217d49621eb99ecf196eb8e0f
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2024-02-08 22:13:16 +0100

    feat: Added config and implemented base structure of cli script

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,42 @@
+use std::fs;
+use std::path::{Path, PathBuf};
+
+use artifact_cleaner::{delete_all_artifact, find_dirs};
 use clap::{Parser, ValueEnum};
+use directories::UserDirs;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    py: ProfileConfig,
+}
+
+#[derive(Deserialize, Debug)]
+struct ProfileConfig {
+    artifact_names: Vec<String>,
+}
+
+impl Config {
+    fn new() -> Self {
+        Self {
+            py: ProfileConfig {
+                artifact_names: vec![String::from("__pycache__")],
+            },
+        }
+    }
+}
 
 /// Tool for cleaning artifacts of programming languages
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     root: std::path::PathBuf,
 
     #[arg(value_enum)]
     profile: Profile,
+
+    #[arg(short, long)]
+    dry_run: bool,
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
@@ -15,7 +44,37 @@ enum Profile {
     Py,
 }
 
+fn get_config() -> Config {
+    let user_dir = UserDirs::new();
+    if let Some(dir) = user_dir {
+        let config_data = fs::read_to_string(dir.home_dir().join(".artifact_cleaner.toml"));
+        if let Ok(file) = config_data {
+            toml::from_str(&file).expect("Invalid toml config file")
+        }
+    }
+    Config::new()
+}
+
 fn main() {
     let args = Args::parse();
-    println!("pattern: {:?}, path: {:?}", args.root, args.profile)
+    dbg!(&args);
+
+    let config: Config = get_config();
+    dbg!(&config);
+
+    let mut findings: Vec<PathBuf> = Vec::new();
+
+    match find_dirs(
+        &mut findings,
+        args.root.as_path(),
+        &config.py.artifact_names,
+        5,
+    ) {
+        Ok(_) => println!("Done"),
+        Err(e) => println!("Error: {:?}", e),
+    }
+    dbg!(&findings);
+    if findings.len() > 0 && !args.dry_run {
+        delete_all_artifact(&findings).unwrap();
+    }
 }
